cd920fcc0b7e539de9a3274824e74d3f1100954a
enzzodasul <enzzodasul@gmail.com>
2026-06-09 02:37:34 -0300

    adição dehover e melhoria em botões , ta,mbem foi adicionado a logo e hover na logo , tambem 3 cards tematicos ,e botão funcional de reservar mesa

---
+++ script.js
@@ -0,0 +1,7 @@
+
+const menuBtn = document.getElementById("menuBtn");
+const navLinks = document.getElementById("navLinks");
+
+menuBtn.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});

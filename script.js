624d02486e3c8fd927777a5e1bd4215a31a77c76
enzzodasul <enzzodasul@gmail.com>
2026-06-09 04:11:17 -0300

    adição de card historia e centralização colocado lado a lado com outro card

--- script.js
+++ script.js
@@ -5,3 +5,36 @@ const navLinks = document.getElementById("navLinks");
 menuBtn.addEventListener("click", () => {
     navLinks.classList.toggle("active");
 });
+
+
+
+
+const cards = document.querySelectorAll('.card-cardapio');
+
+function updateCenterCard() {
+
+    const center = window.innerWidth / 2;
+
+    cards.forEach(card => {
+
+        const rect = card.getBoundingClientRect();
+
+        const cardCenter = rect.left + rect.width / 2;
+
+        const distance = Math.abs(center - cardCenter);
+
+        if(distance < 200){
+            card.style.transform = "scale(1.08)";
+            card.style.opacity = "1";
+        }else{
+            card.style.transform = "scale(0.9)";
+            card.style.opacity = "0.6";
+        }
+
+    });
+}
+
+document.querySelector('.cards')
+.addEventListener('scroll', updateCenterCard);
+
+updateCenterCard();
